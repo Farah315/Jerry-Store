0206365eb7752618bd703b798409e717ba50e927
Farah315 <fara734isgreat@gmail.com>
2025-09-01 11:14:27 +0300

    feat: add project file

---
+++ app/src/main/java/com/fara7/tom_and_jerry/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.fara7.tom_and_jerry
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+    }
+}
