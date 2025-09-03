40c515465e74d21a57217aad09f22fd822c041ef
Farah315 <fara734isgreat@gmail.com>
2025-09-03 18:20:57 +0300

    feat: Update layout and themes for improved edge-to-edge display

--- app/src/main/java/com/fara7/tom_and_jerry/MainActivity.kt
+++ app/src/main/java/com/fara7/tom_and_jerry/MainActivity.kt
@@ -3,11 +3,16 @@ package com.fara7.tom_and_jerry
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        WindowCompat.getInsetsController(window, window.decorView).apply {
+            isAppearanceLightNavigationBars = true
+            isAppearanceLightStatusBars = true
+        }
         setContentView(R.layout.activity_main)
     }
 }
