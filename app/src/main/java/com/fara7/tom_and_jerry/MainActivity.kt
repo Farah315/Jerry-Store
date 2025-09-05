7be15846663da7bea068e765528aad308d73f58a
Farah315 <fara734isgreat@gmail.com>
2025-09-05 15:17:57 +0300

    feat: Add new layout and drawable resources for enhanced UI design

--- app/src/main/java/com/fara7/tom_and_jerry/MainActivity.kt
+++ app/src/main/java/com/fara7/tom_and_jerry/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.fara7.tom_and_jerry
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.View
 import android.widget.ScrollView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsControllerCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,6 +18,19 @@ class MainActivity : AppCompatActivity() {
         }
         setContentView(R.layout.cry)
         val scrollView = findViewById<ScrollView>(R.id.scrollable)
+        setupTransparentSystemBars()
         scrollView.systemBarsPadding()
+    }
+    private fun setupTransparentSystemBars() {
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+
+        window.statusBarColor = Color.TRANSPARENT
+
+        window.navigationBarColor = Color.TRANSPARENT
+
+        WindowInsetsControllerCompat(window, window.decorView).let { controller ->
+            controller.isAppearanceLightStatusBars = true
+            controller.isAppearanceLightNavigationBars = true
+        }
     }
 }
